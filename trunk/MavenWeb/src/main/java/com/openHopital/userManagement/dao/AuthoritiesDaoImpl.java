2c262f43055230a675d11c544cbfcc9dcc032b80
skumar312@gmail.com <skumar312@gmail.com@813686f9-e760-e8b8-7a2a-9ee7b3fee5a4>
2012-06-04 12:59:39 +0000

    generic dao implemented with Oracle

---
+++ trunk/MavenWeb/src/main/java/com/openHopital/userManagement/dao/AuthoritiesDaoImpl.java
@@ -0,0 +1,7 @@
+package com.openHopital.userManagement.dao;
+
+import com.openHopital.userManagement.entity.Authorities;
+
+public class AuthoritiesDaoImpl extends GenericDaoJpaImpl<Authorities> implements AuthoritiesDao{
+
+}
--- trunk/MavenWeb/src/main/java/com/openHopital/userManagement/dao/GenericDaoJpaImpl.java
+++ trunk/MavenWeb/src/main/java/com/openHopital/userManagement/dao/GenericDaoJpaImpl.java
@@ -1,41 +1,65 @@
 package com.openHopital.userManagement.dao;
 
-import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Map;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
-public class GenericDaoJpaImpl<T, PK extends Serializable> implements
-		GenericDao<T, PK> {
 
-	protected Class<T> entityClass;
+public abstract class GenericDaoJpaImpl<T> implements GenericDao<T> {
 
-	@PersistenceContext
-	protected EntityManager entityManager;
+    @PersistenceContext
+    protected EntityManager entityManager;
 
-	@SuppressWarnings("unchecked")
-	public GenericDaoJpaImpl() {
-		ParameterizedType genericSuperclass = (ParameterizedType) getClass()
-				.getGenericSuperclass();
-		this.entityClass = (Class<T>) genericSuperclass
-				.getActualTypeArguments()[0];
+    private Class<T> type;
+
+    public GenericDaoJpaImpl() {
+        Type t = getClass().getGenericSuperclass();
+      
+        ParameterizedType pt = (ParameterizedType) t;
+        System.out.println(pt.getActualTypeArguments()[0].getClass().getName());
+        type = (Class) pt.getActualTypeArguments()[0];
+    }
+
+    public long countAll(final Map<String, Object> params) {
+
+        final StringBuffer queryString = new StringBuffer(
+                "SELECT count(o) from ");
+
+        queryString.append(type.getSimpleName()).append(" o ");
+        //queryString.append(this.getQueryClauses(params, null));
+
+        final Query query = this.entityManager.createQuery(queryString.toString());
+
+        return (Long) query.getSingleResult();
+
+    }
+
+    public T create(final T t) {
+        this.entityManager.merge(t);
+        return t;
+    }
+
+    public void delete(final Object id) {
+        this.entityManager.remove(this.entityManager.getReference(type, id));
+    }
+
+    public T find(final Object id) {
+        return (T) this.entityManager.find(type, id);
+    }
+
+    public T update(final T t) {
+        return this.entityManager.merge(t);    
+    }
+
+	public EntityManager getEntityManager() {
+		return entityManager;
 	}
 
-	public T create(T t) {
-		this.entityManager.persist(t);
-		return t;
-	}
-
-	public T read(PK id) {
-		return this.entityManager.find(entityClass, id);
-	}
-
-	public T update(T t) {
-		return this.entityManager.merge(t);
-	}
-
-	public void delete(T t) {
-		t = this.entityManager.merge(t);
-		this.entityManager.remove(t);
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
 	}
 }
